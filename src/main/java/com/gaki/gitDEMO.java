ac0a909758557fd7298657cc28c29fbfc9274f5c
emory <Arapekah@gmail.com>
2021-04-28 11:04:31 +0800

    1.second commit

--- src/main/java/com/gaki/gitDEMO.java
+++ src/main/java/com/gaki/gitDEMO.java
@@ -3,5 +3,6 @@ package com.gaki;
 public class gitDEMO {
     public static void main(String[] args) {
         System.out.println("Talk is ches show me the code");
+        System.out.println("linux");
     }
 }
